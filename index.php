b0542ffbe9cbba3402ca1b3694ec8e166c6b7aad
Robson Mendes <robsonmrlmendes@gmail.com>
2018-01-21 01:26:05 -0200

    primeira subida

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use \Totclean\Page;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function()
+{    
+    $page = new Page();
+    $page->setTpl("index");
+});
+
+$app->run();
+
+ ?>
---
+++ views-cache/index.2f7b7223290bb342bcf5c190fbc03761.rtpl.php
@@ -0,0 +1,395 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="desktop-menu menu-side-w menu-activated-on-click">
+    <div class="logo-w">
+      <a class="logo" href="index.html"><img src="../site/resources/img/logo.png"><span>TotClean</span></a>
+    </div>
+    <div class="menu-and-user">
+      <div class="logged-user-w">
+        <div class="avatar-w">
+          <img alt="" src="../site/resources/img/avatar1.jpg">
+        </div>
+        <div class="logged-user-info-w">
+          <div class="logged-user-name">
+            Usuário
+          </div>
+          <div class="logged-user-role">
+            Cargo
+          </div>
+        </div>
+        <div class="logged-user-menu">
+          <div class="avatar-w">
+            <img alt="" src="../site/resources/img/avatar1.jpg">
+          </div>
+          <div class="logged-user-info-w">
+            <div class="logged-user-name">
+              Usuário
+            </div>
+            <div class="logged-user-role">
+              Cargo
+            </div>
+          </div>
+          <div class="bg-icon">
+            <i class="dripicons-gear"></i>
+          </div>
+          <ul>
+            <li>
+              <a href="#"><i class="dripicons-checkmark"></i><span>Meus clientes</span></a>
+            </li>
+            <li>
+              <a href="users_profile_big.html"><i class="dripicons-wallet"></i><span>Meus bônus</span></a>
+            </li>
+            <li>
+              <a href="users_profile_small.html"><i class="dripicons-gear"></i><span>Meu perfil</span></a>
+            </li>
+            <li>
+              <a href="#"><i class="dripicons-power"></i><span>Sair</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <ul class="main-menu">
+        <li class="has-sub-menu">
+          <a href="index.html">
+            <div class="icon-w">
+              <div class="dripicons-graph-pie"></div>
+            </div>
+            <span>Dashboard</span></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="#">Financeiro</a>
+            </li>
+            <li>
+              <a href="#">Contas a pagar</a>
+            </li>
+            <li>
+              <a href="#">Contas a receber</a>
+            </li>
+          </ul>
+        </li>
+        <li class="has-sub-menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-cart"></div>
+            </div>
+            <span>Clientes</span></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="users_profile_big.html">Lista de clientes</a>
+            </li>
+            <li>
+              <a href="users_profile_small.html">Cadastro de clientes</a>
+            </li>
+          </ul>
+        </li>
+        <li class="has-sub-menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-clipboard"></div>
+            </div>
+            <span>Fornecedores</span></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="apps_email.html">Lista de fornecedores</a>
+            </li>
+            <li>
+              <a href="apps_projects.html">Cadastro de fornecedores</a>
+            </li>
+          </ul>
+        </li>
+        <li class="has-sub-menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-list"></div>
+            </div>
+            <span>Produtos</span></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="misc_invoice.html">Lista de produtos</a>
+            </li>
+            <li>
+              <a href="misc_charts.html">Cadastro de produtos</a>
+            </li>
+          </ul>
+        </li>
+        <li class="has-sub-menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-rocket"></div>
+            </div>
+            <span>Funcionário</span></a>
+          <ul class="sub-menu">
+            <li>
+              <a href="forms_regular.html">Lista de funcionários</a>
+            </li>
+            <li>
+              <a href="forms_validation.html">Cadastro de funcionários</a>
+            </li>
+          </ul>
+        </li>
+        <li class="menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-gear"></div>
+            </div>
+            <span>Meu perfil</span></a>
+        </li>
+        <li class="menu">
+          <a href="#">
+            <div class="icon-w">
+              <div class="dripicons-power"></div>
+            </div>
+            <span>Sair</span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!--FIM MENU DESKTOP-->
+  <!--INI MENU SUPERIOR-->  
+  <div class="content-w">
+    <ul class="breadcrumb">
+      <li class="breadcrumb-item">
+        <span>Olá usuário seja bem vindo ao seu controle de vendas !</span>
+      </li>
+    </ul>
+    <div class="content-panel-toggler">
+      <i class="os-icon os-icon-grid-squares-22"></i><span>Sidebar</span>
+    </div>
+    <div class="content-i">
+      <div class="content-box">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="element-wrapper">
+              <h6 class="element-header">
+                Paínel
+              </h6>
+              <div class="element-content">
+                <div class="row">
+                  <div class="col-sm-4">
+                    <div class="element-box el-tablo">
+                      <div class="label">
+                        Minha meta
+                      </div>
+                      <div class="valor-azul">
+                        100%
+                      </div>
+                      <div class="trending trending-right-basic">
+                        <span>R$ 10.000,00 </span><i class="os-icon os-icon-bar-chart-stats-up"></i>
+                      </div>
+                    </div>
+                  </div>                          
+                  <div class="col-sm-4">
+                    <div class="element-box el-tablo">
+                      <div class="label">
+                        Meta alcançada
+                      </div>
+                      <div class="valor-verde">
+                        50%
+                      </div>
+                      <div class="trending trending-up-basic">
+                        <span>R$ 5.000,00 </span><i class="os-icon os-icon-bar-chart-stats-up"></i>
+                      </div>
+                    </div>
+                  </div>                           
+                  <div class="col-sm-4">
+                    <div class="element-box el-tablo">
+                      <div class="label">
+                        Meta faltante
+                      </div>
+                      <div class="valor-vermelho">
+                        50%
+                      </div>
+                      <div class="trending trending-down-basic">
+                        <span>R$ 5.000,00 </span><i class="os-icon os-icon-bar-chart-stats-up"></i>
+                      </div>
+                    </div>
+                  </div>                           
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--- GRAFICO
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="element-wrapper">
+              <h6 class="element-header">
+                Vendas
+              </h6>
+              <div class="element-box">
+                <div class="os-tabs-w">
+                  <div class="os-tabs-controls">
+                    <ul class="nav nav-tabs smaller">
+                      <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" href="#tab_overview">Overview</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#tab_sales">Sales</a>
+                      </li>
+                    </ul>
+                    <ul class="nav nav-pills smaller hidden-sm-down">
+                      <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#">Today</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" href="#">7 Days</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#">14 Days</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#">Last Month</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="tab-content">
+                    <div class="tab-pane active" id="tab_overview">
+                      <div class="el-tablo">
+                        <div class="label">
+                          Unique Visitors
+                        </div>
+                        <div class="value">
+                          12,537
+                        </div>
+                      </div>
+                      <div class="el-chart-w">
+                        <canvas height="150px" id="lineChart" width="600px"></canvas>
+                      </div>
+                    </div>
+                    <div class="tab-pane" id="tab_sales"></div>
+                    <div class="tab-pane" id="tab_conversion"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> --->
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="element-wrapper">
+              <h6 class="element-header">
+                Lista de atividades
+              </h6>
+              <div class="element-wrapper">
+                  <div class="element-box">
+                      <h5 class="form-header">
+                          Painel de atividades do dia
+                      </h5>
+                      <div class="form-desc">Todos os dias aparecerá empresas para possíveis ligações e negociações.</div>
+                      <div class="table-responsive">
+                          <table class="table table-striped">
+                              <thead>
+                                  <tr>
+                                      <!--<th>Logo</th>-->
+                                      <th>Nome da empresa</th>
+                                      <th>Endereço</th>
+                                      <th>Cidade</th>
+                                      <th>Fone</th>
+                                      <th>Site</th>
+                                      <th class="text-center">
+                                          Status
+                                      </th>
+                                      <th>View</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  <tr>
+                                      <!--<td class="text-center">
+                                          <img alt="" src="../site/resources/img/logo.png" width="25px">
+                                      </td>-->
+                                      <td>Hospital São João Silas</td>
+                                      <td>Rua São José do mar verde, 3567, Alphaville</td>
+                                      <td>Barueri</td>
+                                      <td>41983675</td>
+                                      <td class="text-center"><a href="saojoaopilas.com.br">saojoaopilas.com.br</a></td>
+                                      <td class="text-center">
+                                          <div class="status-pill green" data-title="Baixado" data-toggle="tooltip"></div>
+                                      </td>
+                                      <td class="text-center"><a href="#"><i class="dripicons-preview"></i></a></td>
+                                  </tr>
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content-panel">
+        <div class="content-panel-close">
+          <i class="os-icon os-icon-close"></i>
+        </div>
+        <div class="element-wrapper">
+          <h6 class="element-header">
+            Acesso rápido
+          </h6>
+          <div class="element-box-tp">
+            <div class="el-buttons-list full-width">
+              <a class="btn btn-white btn-sm" href="#">
+                  <i class="os-icon os-icon-tasks-checked"></i><span>visualizar boletos</span>
+              </a>
+              <a class="btn btn-white btn-sm" href="#">
+                  <i class="os-icon os-icon-bar-chart-stats-up"></i><span>bonus de vendas</span>
+              </a>
+              <a class="btn btn-white btn-sm" href="#">
+                  <i class="os-icon os-icon-calendar-time"></i><span>solicitações</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="element-wrapper">
+          <h6 class="element-header">
+            Info. rápidas
+          </h6>
+          <div class="element-box-tp">
+            <div class="profile-tile">
+              <div class="profile-tile-box">
+                <div class="pt-avatar-w">
+                  <i class="dripicons-cart"></i>
+                </div>
+                <div class="pt-user-name">
+                  Clientes
+                </div>
+              </div>
+              <div class="profile-tile-meta">
+                <ul>
+                  <li>
+                    vendas:<strong>1459</strong>
+                  </li>
+                  <li>
+                    Lider vendas:<strong>Robson Mendes</strong>
+                  </li>
+                </ul>
+                <div class="pt-btn">
+                  <a class="btn btn-primary btn-sm" href="#">clientes</a>
+                </div>
+              </div>
+            </div>
+            <div class="profile-tile">
+              <div class="profile-tile-box">
+                <div class="pt-avatar-w">
+                  <i class="dripicons-list"></i>
+                </div>
+                <div class="pt-user-name">
+                  Produtos
+                </div>
+              </div>
+              <div class="profile-tile-meta">
+                <ul>
+                  <li>
+                    Produtos:<strong>7986</strong>
+                  </li>
+                  <li>
+                    Maior saida:<strong>Nome do produto</strong>
+                  </li>
+                </ul>
+                <div class="pt-btn">
+                  <a class="btn btn-primary btn-sm" href="#">produtos</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
